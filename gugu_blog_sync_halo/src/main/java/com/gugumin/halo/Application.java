eb7706867efbc07acc20c121d183e45c764c91d6
MinMin <newmailbyminmin@gmail.com>
2023-03-13 01:58:09 +0800

    适配结构变更

---
+++ gugu_blog_sync_halo/src/main/java/com/gugumin/halo/Application.java
@@ -0,0 +1,15 @@
+package com.gugumin.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author minmin
+ * @date 2023/03/13
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
